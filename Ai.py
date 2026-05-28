23da45186f7208566eb99c576a2c8e2b751d6330
Z3R0 <144493047+Z3R0SEC@users.noreply.github.com>
2026-05-28 20:04:46 +0200

    Update Ai.py

--- Ai.py
+++ Ai.py
@@ -1,14 +1,19 @@
 from requests import post
+from flask import Flask, jsonify, jsonify
 
-headers = { "X-API-KEY": "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", "MinatoBot/1.0": "Mozilla/5.0", "referer": "https://www.facebook.com/profile.php?id=100094118835962" }
+app = Flask(__name__)
+
+headers = { "X-API-KEY": "Your real api key from mothadev", "User-Agent": "Mozilla/5.0", "referer": "your website link Not facebook" }
 
 message = "Hello Ai, HowreYou ?"
 
 system = "Your Name is Cris st. You are an ai assistance Developed by Cris"
 
-data = { "user_id": "Chris", "messages": { "system": system, "user": user }
-
-req = post("https://api.motadev.xyz/api/chat", json=data, headers=headers).json()
-
-
-print(req)
+@app.route("/", methods=["POST", "GET"])
+def ai():
+    data = { "user_id": "Chris", "messages": { "system": system, "user": user }
+    req = post("https://api.motadev.xyz/api/chat", json=data, headers=headers)
+    print(req.json())
+    return req.json()
+  
+app.run()
